8857f578a7264e846cbd18a110143a1646b9d3ca
Willian Per <wsresilienttasteshopw@gmail.com>
2022-02-10 17:18:52 -0400

    Abordando API com Express parte 3

--- projeto/src/servidor.js
+++ projeto/src/servidor.js
@@ -2,7 +2,10 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParse = require('body-parser')
 const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParse.urlencoded({ extended: true }))
 
 app.get('/produtos', (req, res, next) => {
     res.send(bancoDeDados.getProdutos())
@@ -11,11 +14,25 @@ app.get('/produtos/:id', (req, res, next) => {
     res.send(bancoDeDados.getProduto(req.params.id))
 })
 
-app.get('/produtos', (req, res, next) => {
+app.post('/produtos', (req, res, next) => {
     const produto = bancoDeDados.salvarProduto({
         nome: req.body.nome,
         preco: req.body.preco
     })
+    res.send(produto) //JSON
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) //JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
     res.send(produto) //JSON
 })
 
